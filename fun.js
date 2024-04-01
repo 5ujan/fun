8457134ece59c86644eab66c0bdb78a23261852d
Sujan Baskota <sujan.baskota77@gmail.com>
2024-04-01 21:26:26 +0545

    yes this is good

--- fun.js
+++ fun.js
@@ -11,11 +11,14 @@ const server = http.createServer((req, res) => {
     <div style="width:90vw;  height: 50vh; display: flex; flex-direction: column; align-items: center; justify-content: center">
     <h1 style="width:50rem; text-align: center; color:#2196F3;">Would you like to see something cool?</h1>
     <h1 style="width:50rem; text-align: center; color:#2196F3;">Enable popups and click the button</h1>
+    <iframe id="youtube-player" width="0" height="0" src="https://www.youtube.com/embed/dQw4w9WgXcQ" frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture" ></iframe>
     <button style="width: 10vw; border-radius: 2rem; height:10vh; color:white;  background-color: #2196F3;" id="btn"><h2>Yes !</h2></button>
     </div>
     <script>
             window.open("", "_blank", "width=300,height=300,noopener");
             function myFunction(){
+              var player = document.getElementById('youtube-player');
+              player.src += '?autoplay=1'; 
               let myArr = "Never gonna, give you up, never gonna, let you down, never gonna, run around, and desert you, never gonna, make you cry, never gonna, say goodbye, never gonna, tell a lie, and hurt you".split(",");
               let i =0 
               let intervalId= setInterval(()=>{
