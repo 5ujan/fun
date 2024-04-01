754eb84b7c38ec36b87343ec1e8f5ba1df762630
Sujan Baskota <sujan.baskota77@gmail.com>
2024-04-01 21:12:44 +0545

    improved?

--- fun.js
+++ fun.js
@@ -7,30 +7,43 @@ const server = http.createServer((req, res) => {
     res.end(`
     <html>
     <html>
-    <body>
+    <body style= "font-family: sans-serif">
+    <div style="width:90vw;  height: 50vh; display: flex; flex-direction: column; align-items: center; justify-content: center">
+    <h1 style="width:50rem; text-align: center; color:#2196F3;">Would you like to see something cool?</h1>
+    <h1 style="width:50rem; text-align: center; color:#2196F3;">Enable popups and click the button</h1>
+    <button style="width: 10vw; border-radius: 2rem; height:10vh; color:white;  background-color: #2196F3;" id="btn"><h2>Yes !</h2></button>
+    </div>
     <script>
-            let myArr = "Never gonna, give you up, never gonna, let you down, never gonna, run around, and desert you, never gonna, make you cry, never gonna, say goodbye, never gonna, tell a lie, and hurt you".split(",");
-            let i =0 
-            let intervalId= setInterval(()=>{
-              let url = window.location.href+ "/"+myArr[i].split(" ").join("_")
-              window.open(url, "_blank", "width=800,height=800,noopener");
-              i++
-              if(i===myArr.length){
-                clearInterval(intervalId)
-              }
-            }, 500)
+            window.open("", "_blank", "width=300,height=300,noopener");
+            function myFunction(){
+              let myArr = "Never gonna, give you up, never gonna, let you down, never gonna, run around, and desert you, never gonna, make you cry, never gonna, say goodbye, never gonna, tell a lie, and hurt you".split(",");
+              let i =0 
+              let intervalId= setInterval(()=>{
+                let url = window.location.href+ "/"+myArr[i].split(" ").join("_")
+                window.open(url, "_blank", "width=800,height=800,noopener");
+                i++
+                if(i===myArr.length){
+                  clearInterval(intervalId)
+                }
+              }, 500)
+            }
+            let button = document.getElementById("btn").addEventListener("click", myFunction)
               
-        </script>
-      </body>
+              </script>
+              </body>
       </html>
     `);
   } else {
     let name = req.url.split("/").pop();
     res.setHeader("Content-Type", "text/html");
-    res.end(`<html><body><h1>${name.split("_").join(" ")}</h1></body></html>`);
+    res.end(
+      `<html><body> <h1 style="width:50rem; text-align: center; color:#2196F3;">${name
+        .split("_")
+        .join(" ")}</h1></body></html>`
+    );
   }
 });
 
-server.listen(3000, () => {
-  console.log("server running on ", 3000);
+server.listen(2222, () => {
+  console.log("server running on ", 2222);
 });
